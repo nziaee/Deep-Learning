Change the file name and fix backpropagation

Project1-Bikesharing.py
@@ -1,6 +1,7 @@
+
 		#sigmoid ativation function
         self.activation_function = lambda x : 1/(1+np.exp(-x))  # Replace 0 with your sigmoid calculation.
-        
+
         #Forward pass hidden layer 
         hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
         hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
@@ -14,20 +15,20 @@
         
         # Backpropagated error terms
         output_error_term = error * final_outputs * (1-final_outputs)
-        
-        # Calculate the hidden layer's contribution to the error
-        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output)
 
+        # Calculate the hidden layer's contribution to the error
+        hidden_error = np.dot(self.weights_hidden_to_output, output_error_term)
+        
         hidden_error_term = hidden_error * hidden_outputs * (1-hidden_outputs)
         
         # Weight step (input to hidden)
-        delta_weights_i_h += learnrate * hidden_error_term * X[:, None]
+        delta_weights_i_h += self.lr * hidden_error_term * X[:, None]
         # Weight step (hidden to output)
-        delta_weights_h_o += learnrate * output_error_term * hidden_outputs
-        
-#########################################################
-# Set your hyperparameters here
-##########################################################
+        delta_weights_h_o += self.lr *  hidden_outputs * output_error_term.T
+
+####################################
+# hyperparameters 
+####################################
 iterations = 100
 learning_rate = 0.1
 hidden_nodes = 2