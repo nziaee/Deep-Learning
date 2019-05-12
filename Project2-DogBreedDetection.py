b24c9d4cbe5b3ffa9ccdd06fcdbef5aeb3518b0c
Nikki <n.ziaee20@yahoo.com>
2019-05-12 08:49:59 -0500

    Load the data, define the CNN model, define the loss function and optimizer

--- Project2-DogBreedDetection.py
+++ Project2-DogBreedDetection.py
@@ -40,3 +40,70 @@ def VGG16_predict(img_path):
     print(top_p)
     print(top_c)
     return top_c # predicted class index
+	
+def dog_detector(img_path):
+    ## TODO: Complete the function.
+    output = VGG16_predict(img_path)
+    if 151<= output <=268:
+        return True
+    return False
+	
+#Load data for training and validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'valid']}
+loaders_scratch = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=0)
+              for x in ['train', 'valid']}
+
+#Define the CNN model			  
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(28*28*64, 2048)
+        self.fc2 = nn.Linear(2048, 133)
+        self.dropout = nn.Dropout(0.25)
+		
+    def forward(self, x):
+        #224*224*3
+        x = self.pool(F.relu(self.conv1(x)))
+        #112*112*16
+        x = self.pool(F.relu(self.conv2(x)))
+        #56*56*32
+        x = self.pool(F.relu(self.conv3(x)))
+        #28*28*64
+        x = x.view(-1, 28*28*64)
+        x = x.self.dropout(x)
+        x = F.relu(x.fc1(x))
+        x = x.self.dropout(x)
+        x = x.fc2(x)
+        return x
+		
+#Define the loss function and optimizer
+import torch.optim as optim
+
+riterion_scratch = nn.CrossEntropyLoss()
+optimizer_scratch = optim.SGD(model_scratch.parameters(), lr=0.01)
