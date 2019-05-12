9bf9fc677fc673fa3a2834f09e9dda0855363c25
nziaee <n.ziaee20@yahoo.com>
2019-05-11 20:00:33 -0500

    Define the activation function, forward pass and backpropagation, and hyperparameters

---
+++ v1.py
@@ -0,0 +1,34 @@
+		#sigmoid ativation function
+        self.activation_function = lambda x : 1/(1+np.exp(-x))  # Replace 0 with your sigmoid calculation.
+        
+        #Forward pass hidden layer 
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
+
+        #Forward pass output layer
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+        final_outputs = self.activation_function(final_inputs) # signals from final output layer
+
+        # Backward pass output error
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
+        
+        # Backpropagated error terms
+        output_error_term = error * final_outputs * (1-final_outputs)
+        
+        # Calculate the hidden layer's contribution to the error
+        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output)
+
+        hidden_error_term = hidden_error * hidden_outputs * (1-hidden_outputs)
+        
+        # Weight step (input to hidden)
+        delta_weights_i_h += learnrate * hidden_error_term * X[:, None]
+        # Weight step (hidden to output)
+        delta_weights_h_o += learnrate * output_error_term * hidden_outputs
+        
+#########################################################
+# Set your hyperparameters here
+##########################################################
+iterations = 100
+learning_rate = 0.1
+hidden_nodes = 2
+output_nodes = 1
